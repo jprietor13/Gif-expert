02b7df6ed3fb632fc2cc640d67ae2304cf94bd38
Juan Prieto <jp1739@gmail.com>
2021-09-04 11:28:58 -0500

    feat: Create category list example

--- src/components/GifExpertApp.jsx
+++ src/components/GifExpertApp.jsx
@@ -1,10 +1,23 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const GifExpertApp = () => {
+  const categories = ["Bleach", "Dragon Ball Z", "Naruto"];
+  const [categorie, setCategorie] = useState(categories);
+
+  const handleAddCategorie = () => {
+    const newCategorie = "Samurai X";
+    setCategorie([...categories, newCategorie])
+  }
   return (
     <div>
       <h2>GifExpertApp</h2>
       <hr />
+      <button type="button" onClick={handleAddCategorie}>Agregar</button>
+      <ol>
+        {categorie.map(category =>
+          <li key={category}>{category}</li>
+        )}
+      </ol>
     </div>
   )
 }
