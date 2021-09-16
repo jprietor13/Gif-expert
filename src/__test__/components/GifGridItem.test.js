6d1f02f341bf4e47bfc14717b4f791137e4a9730
Juan Prieto <jp1739@gmail.com>
2021-09-15 21:18:07 -0500

    feat: Others test in GifGridItem component

--- src/__test__/components/GifGridItem.test.js
+++ src/__test__/components/GifGridItem.test.js
@@ -5,9 +5,26 @@ import { GifGridItem } from '../../components/GifGridItem';
 describe("Pruebas en GifGridItem", () => {
   const title = "title categorie";
   const url = "https://localhost:3000/title_categorie.jpg";
+  const wrapper = shallow(<GifGridItem title={title} url={url} />);
 
   test("Debe mostrar el componente", () => {
-    const wrapper = shallow(<GifGridItem title={title} url={url} />);
     expect(wrapper).toMatchSnapshot();
   })
+
+  test("Debe tener un parrafo con el titulo", () => {
+    const parrafo = wrapper.find('p');
+    expect(parrafo.text().trim()).toBe(title);
+  });
+
+  test("Debe tener el url y el titulo de la imagen igual al de los props", () => {
+    const img = wrapper.find('img');
+    //console.log(img.prop('src'))
+    expect(img.prop('src')).toBe(url); //props sirve para ver los atributos del elemento html
+    expect(img.prop('alt')).toBe(title);
+  });
+
+  test("Debe tener el div padre la clase animate__backInDown", () => {
+    const div = wrapper.find('div');
+    expect(div.prop('className')).toContain('animate__backInDown')
+  });
 })
